729729941180ed86ba22fbd859410016db6d2f29
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-03-07 02:08:41 +0900

    pagination 1~5, 6~10으로 나누고 이전 다음 버튼 생성

--- admin-toggle/src/components/user-page/pagination.jsx
+++ admin-toggle/src/components/user-page/pagination.jsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 /**
@@ -12,25 +13,91 @@ import styled from "styled-components";
  */
 const Pagination = ({ userPerPage, userData, sort }) => {
     const navigate = useNavigate();
+    const { pageNumber } = useParams();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [page, setPage] = useState(1);
+    const perPageGroup = 5;
+
+    useEffect(() => {
+        if (pageNumber <= perPageGroup) {
+            setCurrentPage(1);
+        }
+        if (pageNumber > perPageGroup) {
+            setCurrentPage(2);
+        }
+    }, [pageNumber]);
+
+    // userData.length를 userPerPage로 나누어서 올림
+    const pageCount = Math.ceil(userData.length / userPerPage);
+
+    // pageCount를 perPageGroup로 나눔
+    const totalPageGroup = Math.ceil(pageCount / perPageGroup);
 
     // 받아온 값으로 새로운 주소로 이동
     const paginate = (page) => {
         navigate(`/user/${page}?per_page=${userPerPage}&sort=${sort}`);
     };
 
-    // userData.length를 userPerPage로 나누어서 올림
-    const pageCount = Math.ceil(userData.length / userPerPage);
+    const pageNumbers = () => {
+        const startPage = (currentPage - 1) * perPageGroup + 1;
+        const endPage = Math.min(startPage + perPageGroup - 1, pageCount);
+
+        return Array.from({ length: endPage - startPage + 1 }, (_, index) => (
+            <Li key={index}>
+                <Button
+                    onClick={() => {
+                        paginate(index + startPage);
+                        setPage(index + startPage);
+                    }}
+                >
+                    {index + startPage}
+                </Button>
+            </Li>
+        ));
+    };
+
+    const prevPage = () => {
+        if (pageNumber > 1) {
+            const newPage = pageNumber - 1;
+            paginate(newPage);
+            setPage(newPage);
+        }
+    };
+
+    const prevGroup = () => {
+        paginate(1);
+        setCurrentPage(1);
+        setPage(1);
+    };
+
+    const nextPage = () => {
+        if (pageNumber < pageCount) {
+            const newPage = page + 1;
+            paginate(newPage);
+            setPage(newPage);
+        }
+    };
+
+    const nextGroup = () => {
+        setCurrentPage(totalPageGroup);
+    };
 
     return (
         <PageNumber>
             <Ul>
-                {Array.from({ length: pageCount }, (_, index) => (
-                    <Li key={index}>
-                        <Button onClick={() => paginate(index + 1)}>
-                            {index + 1}
-                        </Button>
-                    </Li>
-                ))}
+                <Li>
+                    <Button onClick={prevGroup}>&lt;&lt;</Button>
+                </Li>
+                <Li>
+                    <Button onClick={prevPage}>&lt;</Button>
+                </Li>
+                {pageNumbers()}
+                <Li>
+                    <Button onClick={nextPage}>&gt;</Button>
+                </Li>
+                <Li>
+                    <Button onClick={nextGroup}>&gt;&gt;</Button>
+                </Li>
             </Ul>
         </PageNumber>
     );
