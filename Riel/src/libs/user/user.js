e022249208560f88e92cbff2e357b4cd495a85be
keeprok <140785214+keeprok@users.noreply.github.com>
2024-03-07 00:11:08 +0900

    feat : 데이터 관리 부분 수정했습니다

--- Riel/src/libs/user/user.js
+++ Riel/src/libs/user/user.js
@@ -2,7 +2,7 @@ import { fakerKO as faker } from "@faker-js/faker";
 
 export const userListData = Array.from({ length: 200 }, (_, index) => ({
     id: index + 1,
-    name: faker.person.firstName() + faker.person.lastName(),
+    name: faker.person.lastName() + faker.person.firstName(),
     birthDay: faker.date.birthdate(),
     phone: faker.phone.number(),
     lastLogin: faker.date.recent(),
