cee48692405da91ac1f34fcb990c33699671c136
hayoung78 <wendy4332@naver.com>
2024-04-03 23:53:30 +0900

    fix :코드수정

--- admin-toggle-page/src/components/common/pagenation.js
+++ admin-toggle-page/src/components/common/pagenation.js
@@ -6,32 +6,50 @@ function Pagination({ total, limit, currentPage, curParams, setCurParams }) {
     const numPages = Math.ceil(total / limit);
     const [searchParams, setSearchParams] = useSearchParams();
     const pageGroup = 5;
+    const pageParam = searchParams.get("currentPage");
     // + 일 때와, - 일 때
     const handlePageChange = (page) => {
         searchParams.set("currentPage", page);
         setSearchParams(searchParams);
     };
+
+    //페이지 그룹 시작 : 1,6,11~
+    const startPageGroup =
+        Math.floor((pageParam - 1) / pageGroup) * pageGroup + 1;
+
+    //페이지 그룹 마지막 번호 :
+    const endPageGroup = Math.min(startPageGroup + pageGroup - 1, numPages);
+
     return (
         <ButtonWrapper>
-            <Button color="lemon" size="mini" text="&lt;&lt;" />
+            <Button
+                color="lemon"
+                size="mini"
+                text="&lt;&lt;"
+                onClick={() => handlePageChange(startPageGroup - pageGroup)}
+                disabled={startPageGroup === 1}
+            />
             <Button
                 color="peach"
                 size="mini"
                 text="&lt;"
                 onClick={() => handlePageChange(currentPage - 1)}
-                disabled={currentPage === 1}
+                disabled={startPageGroup === 1}
             />
-            {Array(numPages)
+            {Array(endPageGroup - startPageGroup + 1)
                 .fill()
-                .map((_, i) => (
-                    <Button
-                        color="peach"
-                        size="mini"
-                        text={i + 1}
-                        key={i + 1}
-                        onClick={() => handlePageChange(i + 1)}
-                    />
-                ))}
+                .map((_, i) => {
+                    const pageNumber = startPageGroup + i;
+                    return (
+                        <Button
+                            color="peach"
+                            size="mini"
+                            text={pageNumber.toString()}
+                            key={pageNumber}
+                            onClick={() => handlePageChange(pageNumber)}
+                        />
+                    );
+                })}
             <Button
                 color="peach"
                 size="mini"
@@ -39,7 +57,13 @@ function Pagination({ total, limit, currentPage, curParams, setCurParams }) {
                 onClick={() => handlePageChange(currentPage + 1)}
                 disabled={currentPage === numPages}
             />
-            <Button color="lemon" size="mini" text="&gt;&gt;" />
+            <Button
+                color="lemon"
+                size="mini"
+                text="&gt;&gt;"
+                onClick={() => handlePageChange(endPageGroup + 1)}
+                disabled={endPageGroup === numPages}
+            />
         </ButtonWrapper>
     );
 }
